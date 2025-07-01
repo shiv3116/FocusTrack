6e1be562bda32b9be5dc29374806cf6623f46e33
Shivam Sharma <shiva.shar132001@gmail.com>
2025-07-02 00:41:25 +0530

    Setting up auth service & creating user entity

---
+++ FocusTrack_Backend/services/authservice/src/main/java/com/focustrack/authservice/entity/User.java
@@ -0,0 +1,17 @@
+package com.focustrack.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
