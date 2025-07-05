2d08fe8f18ef29ebfb899fcd1d965110ad2fb168
Shivam Sharma <shiva.shar132001@gmail.com>
2025-07-06 02:07:34 +0530

    Adding JWT Dependencies & JWT Service

---
+++ FocusTrack_Backend/services/authservice/src/main/java/com/focustrack/authservice/service/JwtService.java
@@ -0,0 +1,37 @@
+package com.focustrack.authservice.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtService {
+
+    @Value("${spring.security.jwt.secret}")
+    private static String secretKey;
+
+    @Value("${spring.security.jwt.expiration-ms}")
+    private static String expirationTime;
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+Long.parseLong(expirationTime)))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
