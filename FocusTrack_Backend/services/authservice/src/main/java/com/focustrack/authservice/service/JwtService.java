bb511cca699bc4256428062366ea4145dd5d27cf
Shivam Sharma <shiva.shar132001@gmail.com>
2025-07-12 16:26:45 +0530

    Adding logic for JWT Service: Generating token & Validating token

--- FocusTrack_Backend/services/authservice/src/main/java/com/focustrack/authservice/service/JwtService.java
+++ FocusTrack_Backend/services/authservice/src/main/java/com/focustrack/authservice/service/JwtService.java
@@ -1,23 +1,26 @@
 package com.focustrack.authservice.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 public class JwtService {
 
     @Value("${spring.security.jwt.secret}")
-    private static String secretKey;
+    private String secretKey;
 
     @Value("${spring.security.jwt.expiration-ms}")
-    private static String expirationTime;
+    private String expirationTime;
 
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> extraClaims = new HashMap<>();
@@ -33,5 +36,35 @@ public class JwtService {
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extraAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extraAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private Date getTokenExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return getTokenExpiration(token).before(new Date());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 }
